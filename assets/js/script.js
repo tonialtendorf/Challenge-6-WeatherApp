451a7c866b5e100ace8c9396d21582d32d23a713
tonialtendorf <tvohnoutka05@gmail.com>
2023-01-19 14:27:40 -0600

    api key input

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var APIKey = "eb23e19b9a1a5041787f2931574a32f1"
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+
+var city;
+var state;
+var country;
+
+
+
+
+
+/*
+var cityInputEl = document.getElementById('cityInput');
+var submitBtn = document.getElementById('add');
+var city = document.getElementById('cityOutput');
+var temp = document.getElementById('temp');
