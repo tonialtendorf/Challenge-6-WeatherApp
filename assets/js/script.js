2d477d70b5790957f0ff38d8693fa114a2a72fa2
tonialtendorf <tvohnoutka05@gmail.com>
2023-01-19 14:54:08 -0600

    added dayjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,7 @@
 
-var APIKey = "eb23e19b9a1a5041787f2931574a32f1"
+var APIKey = "f1ababfab952dd503c1fa036d5b7873a"
 var city;
 var state;
 var country;
-var queryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=" + city + state + country + "&appid" + APIKey';
-
-fetch(queryURL)
-
-
-
-
-
-
-/*
-var cityInputEl = document.getElementById('cityInput');
-var submitBtn = document.getElementById('add');
-var city = document.getElementById('cityOutput');
-var temp = document.getElementById('temp');
+//var queryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid" + APIKey';
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
